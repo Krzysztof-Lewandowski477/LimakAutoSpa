a1096f860a41f8dbf0a5054d28092d3e527b48f4
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-11 22:35:11 +0100

    WebConfiguration

--- limakautospa/src/main/java/pl/limakautospa/limakautospa/config/WebConfiguration.java
+++ limakautospa/src/main/java/pl/limakautospa/limakautospa/config/WebConfiguration.java
@@ -9,4 +9,17 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController ( "/login" ).setViewName ( "login" );
     }
+
+
+
+
+//    CREATE DATABASE limak CHARACTER SET  utf8mb4 COLLATE utf8mb4_unicode_ci;
+//
+//    INSERT INTO roles (id,name) VALUES (1, 'ROLE_USER');
+//    INSERT INTO roles (id, name) VALUES (2, 'ROLE_ADMIN');
+//
+//    INSERT INTO users (username,email,password,active) VALUES ('user1', 'email@email.pl','abc',true);
+//    INSERT INTO users_roles (user_id, roles_id) VALUES (1,1);
+//
+//    UPDATE users SET password ='{noop}abc' WHERE username = 'user1';
 }
