9cae56f1b806498123f38f3b90790c88f4bd8750
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-12 13:54:23 +0100

    HomePageController

--- limakautospa/src/main/java/pl/limakautospa/limakautospa/controllers/HomePageController.java
+++ limakautospa/src/main/java/pl/limakautospa/limakautospa/controllers/HomePageController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class HomePageController {
 
-    @GetMapping("/")
+    @GetMapping
     public String getHomePage(){
 
         return "index";
